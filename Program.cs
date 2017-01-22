0f1da056b5cf5ec230f7c6b91ced1f5dc154ac3e
Alex Badjun <alexbadjun@gmail.com>
2017-01-22 01:08:41 -0500

    added new methods

--- Program.cs
+++ Program.cs
@@ -10,23 +10,62 @@ namespace VirtualPet
     {
         static void Main(string[] args)
         {
-            string firstName;
-            string lastName;
+
+            string name;
+            int userInput;
+                   
+            VirtualPet newPet = new VirtualPet();
+
+            Console.WriteLine("Welcome to Mouseimon!\n\nYou will be tasked with keeping your precious mouse alive.\n\n");
 
 
-            Console.WriteLine("Welcome to Mouseimon!\n\nYou will be tasked with keeping he/she alive.\n\n");
-
-
-            Console.WriteLine("Please enter your pet mouses first name");
+            Console.WriteLine("Please enter your pet mouses name \n");
             {
-                firstName = Console.ReadLine();
-            }
-            Console.WriteLine("Please enter your new mouses last name");
-            {
-                lastName = Console.ReadLine();
+                name = Console.ReadLine();
             }
 
+            while (true)
+            {
 
+                Console.WriteLine(name + " the mouse");
+
+                Console.WriteLine(newPet.Hunger + " current hunger level ");
+
+                Console.WriteLine(newPet.Thirst + " current thirst level ");
+
+                Console.WriteLine(newPet.Sickness + " current health level ");
+
+                Console.WriteLine(newPet.Boredom + " current boredom level ");
+
+
+                Console.WriteLine("What do you want to do?");
+
+                Console.WriteLine("\n 1.Feed \n 2.Water \n 3.Rest\n 4.Play ");
+
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput == 1)
+                {
+                    newPet.Food();
+                }
+                else if (userInput == 2)
+                {
+                    newPet.Drink();
+                }
+                else if (userInput == 3)
+                {
+                    newPet.Cold();
+                }
+                else if (userInput == 4)
+                {
+                    newPet.Crazy();
+                }
+                else
+                {
+                    Console.WriteLine(" Please choose one of the above");
+                    userInput = int.Parse(Console.ReadLine());
+                }
+            }
         }
     }
 }
--- VirtualPet.cs
+++ VirtualPet.cs
@@ -15,81 +15,115 @@ namespace VirtualPet
         private int thirst;
         private int sickness;
         private int boredom;
-
-        // properties
+        private bool dead = false;
+        
+        //  auto implemented properties
         public int Hunger
         {
             get { return hunger; }
-            set { hunger = value; }
+           
         }
 
         public int Thirst
         {
             get { return thirst; }
-            set { thirst = value; }
+            
         }
         public int Sickness
         {
             get { return sickness; }
-            set { sickness = value; }
+            
         }
         public int Boredom
         {
             get { return boredom; }
-            set { boredom = value; }
+            
         }
-        ////constructors
-        //public VirtualPet()
-        //{
-        //    this.hunger = "cheese";
-        //    this.thirst = "water";
-        //    this.sickness = "vomit";
-        //    this.boredom = "walk";
-        //}
-        //public VirtualPet(string hunger)
-        //{
-        //    this.hunger = hunger;
-        //    this.thirst = "milk";
-        //    this.sickness = "headache";
-        //    this.boredom = "fetch";
 
-        //}
-        //public VirtualPet(string hunger, string thirst)
-        //{
-        //    this.hunger = hunger;
-        //    this.thirst = thirst;
-        //    this.sickness = "constipated";
-        //    this.boredom = "hide n seek";
-        //}
-        //public VirtualPet(string hunger, string thirst, string sickness)
-        //{
-        //    this.hunger = hunger;
-        //    this.thirst = thirst;
-        //    this.sickness = sickness;
-        //    this.boredom = "frisbee";
+
+        public VirtualPet()
+        {
+            hunger = 5;
+            thirst = 5;
+            sickness = 5;
+            boredom = 5;
+            dead = false;
+        }
 
 
         //method
         public void Food()
         {
-            hunger -= 4;
-            Console.WriteLine(" Eating food " + hunger);
+            hunger -= 2;
+            thirst--;
+            Console.WriteLine(" You fed your mouse" );
         }
         public void Drink()
         {
-            thirst += 2;
-            Console.WriteLine("Sipping on drink" + thirst);
+            thirst -= 2;
+            sickness--;
+            Console.WriteLine("You provided water to your mouse" );
         }
         public void Cold()
         {
-            sickness -= 5;
-            Console.WriteLine("Feeling nauseus " + sickness);
+            sickness -= 2;
+            boredom++;
+            
+            Console.WriteLine("Your mouse is feeling better");
         }
         public void Crazy()
         {
-            boredom += 3;
-            Console.WriteLine(" Going crazy " + boredom);
+            boredom -= 2;
+            hunger++;
+            Console.WriteLine(" Your mouse is less bored");
         }
-    }
+        //public bool Alive()
+        //{
+        //    if (hunger < 0)
+        //    {
+        //        Console.WriteLine(" Your mouse died of starvation");
+        //        return true;
+        //    }
+        //    else if (hunger > 10)
+        //    {
+        //        Console.WriteLine(" You over fed your mouse");
+        //        return true;
+        //    }
+        //    else if (thirst < 0)
+        //    {
+        //        Console.WriteLine(" Your mouse ran out of liquids");
+        //        return true;
+        //    }
+        //    else if (thirst > 10)
+        //    {
+        //        Console.WriteLine(" How could you give him so much water!!!");
+        //        return true;
+        //    }
+        //    else if (sickness < 0)
+        //    {
+        //        Console.WriteLine(" You have died of sickness");
+        //        return true;
+        //    }
+        //    else if (sickness > 10)
+        //    {
+        //        Console.WriteLine(" Couldn't keep your mouse alive! That's a real shame");
+        //        return true;
+        //    }
+        //    else if (boredom < 0)
+        //    {
+        //        Console.WriteLine(" Dang! Poor little guy had no friends");
+        //        return true;
+        //    }
+        //    else if (boredom > 10)
+        //    {
+        //        Console.WriteLine(" Too much socialization is never good");
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        // }
+       // }
+     }
   }
 
